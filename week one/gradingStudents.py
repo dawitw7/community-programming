6b05e769a48d294365812ec49dbf378004f885dd
Dawit Workneh <65712380+dawitw7@users.noreply.github.com>
2022-08-17 21:59:44 +0300

    Create gradingStudents.py

---
+++ week one/gradingStudents.py
@@ -0,0 +1,50 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'gradingStudents' function below.
+#
+# The function is expected to return an INTEGER_ARRAY.
+# The function accepts INTEGER_ARRAY grades as parameter.
+#
+
+def gradingStudents(grades):
+    # Write your code here
+    for i in range(len(grades)):
+        if grades[i] < 38:
+            continue
+        else:
+            num = grades[i]
+            diff = num%5
+            if diff == 3:
+                num = num +2
+                grades[i]=num
+            elif diff ==4:
+                num = num +1
+                grades[i] = num
+            else:
+                continue
+    return grades                
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    grades_count = int(input().strip())
+
+    grades = []
+
+    for _ in range(grades_count):
+        grades_item = int(input().strip())
+        grades.append(grades_item)
+
+    result = gradingStudents(grades)
+
+    fptr.write('\n'.join(map(str, result)))
+    fptr.write('\n')
+
+    fptr.close()
