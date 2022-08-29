d0e50f55cc0fffed970408227f6d897c08517e4f
Dawit Workneh <65712380+dawitw7@users.noreply.github.com>
2022-08-29 18:00:58 +0300

    Create LargestNumber.py

---
+++ week one/LargestNumber.py
@@ -0,0 +1,17 @@
+class Solution:
+    def largestNumber(self, nums: List[int]) -> str:
+        
+        largestnumber =''
+        for i in range(len(nums)-1):
+            for j in range(i+1, len(nums)):
+                temp1 = str(nums[i])+str(nums[j])
+                temp2 = str(nums[j])+str(nums[i])
+                if int(temp2) > int(temp1):
+                    nums[i] , nums[j] = nums[j] , nums[i]
+        for i in range(len(nums)):
+            largestnumber += str(nums[i])
+        if int(largestnumber)>0:
+            return str(largestnumber)
+        else:
+            return '0'
+        
